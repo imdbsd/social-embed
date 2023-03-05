8ee0c528e37d177dfe862b9d8898fa4e8729bf34
Surya Darma <budisuryadarma0407@gmail.com>
2023-03-05 15:13:04 +0800

    adding error embed

--- packages/playground-cra/src/App.tsx
+++ packages/playground-cra/src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import {TiktokEmbed} from '@imdbsd/tiktok-embed';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: 'flex', justifyContent: 'center', margin: 20}}>
+      <TiktokEmbed url="https://www.tiktok.com/@caasdasd535302" />
     </div>
   );
 }
--- packages/tiktok-embed/src/TiktokEmbed.tsx
+++ packages/tiktok-embed/src/TiktokEmbed.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import useTiktokOembed from './useTiktokOembed';
+import ErrorPlaceholder from './ErrorPlaceholder';
 
 export type Props = {
   url: string;
@@ -8,7 +9,18 @@ export type Props = {
 const TiktokEmbed: React.FC<Props> = (props) => {
   const {loading, data, error} = useTiktokOembed({url: props.url});
 
-  return null;
+  if (loading) {
+    return null;
+  }
+
+  if (!data || error) {
+    if (error) {
+      console.error(error);
+    }
+    return <ErrorPlaceholder url={props.url} />;
+  }
+
+  return <div dangerouslySetInnerHTML={{__html: data.html}} />;
 };
 
 export default TiktokEmbed;
